a2b38db8a3b4a3d13bb03ccf341e9fd4fba20ee0
EAD Fritz <ead.fritz@gmail.com>
2015-02-14 15:42:07 +0100

    re-indent and format. Does not do anything yet

--- user/user_main.c
+++ user/user_main.c
@@ -1,32 +1,32 @@
 /* main.c -- MQTT client example
-*
-* Copyright (c) 2014-2015, Tuan PM <tuanpm at live dot com>
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*
-* * Redistributions of source code must retain the above copyright notice,
-* this list of conditions and the following disclaimer.
-* * Redistributions in binary form must reproduce the above copyright
-* notice, this list of conditions and the following disclaimer in the
-* documentation and/or other materials provided with the distribution.
-* * Neither the name of Redis nor the names of its contributors may be used
-* to endorse or promote products derived from this software without
-* specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-* POSSIBILITY OF SUCH DAMAGE.
-*/
+ *
+ * Copyright (c) 2014-2015, Tuan PM <tuanpm at live dot com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * * Neither the name of Redis nor the names of its contributors may be used
+ * to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 #include "ets_sys.h"
 #include "osapi.h"
 #include "mqtt.h"
@@ -50,58 +50,59 @@ static void setup(void);
 void ICACHE_FLASH_ATTR
 wifiConnectCb(uint8_t status) {
 
-	if(status == STATION_GOT_IP){
-		MQTT_Connect(&mqttClient);
-	} else {
-		MQTT_Disconnect(&mqttClient);
-	}
+  if (status == STATION_GOT_IP) {
+    MQTT_Connect(&mqttClient);
+  } else {
+    MQTT_Disconnect(&mqttClient);
+  }
 }
 
 void ICACHE_FLASH_ATTR
 mqttConnectedCb(uint32_t *args) {
 
-	MQTT_Client* client = (MQTT_Client*)args;
-	INFO("MQTT: Connected\r\n");
-	MQTT_Subscribe(client, "/mqtt/topic/0", 0);
-	MQTT_Subscribe(client, "/mqtt/topic/1", 1);
-	MQTT_Subscribe(client, "/mqtt/topic/2", 2);
+  MQTT_Client* client = (MQTT_Client*) args;
+  INFO("MQTT: Connected\n");
+  MQTT_Subscribe(client, "/mqtt/topic/0", 0);
+  MQTT_Subscribe(client, "/mqtt/topic/1", 1);
+  MQTT_Subscribe(client, "/mqtt/topic/2", 2);
 
-	MQTT_Publish(client, "/mqtt/topic/0", "hello0", 6, 0, 0);
-	MQTT_Publish(client, "/mqtt/topic/1", "hello1", 6, 1, 0);
-	MQTT_Publish(client, "/mqtt/topic/2", "hello2", 6, 2, 0);
+  MQTT_Publish(client, "/mqtt/topic/0", "hello0", 6, 0, 0);
+  MQTT_Publish(client, "/mqtt/topic/1", "hello1", 6, 1, 0);
+  MQTT_Publish(client, "/mqtt/topic/2", "hello2", 6, 2, 0);
 
 }
 
 void ICACHE_FLASH_ATTR
 mqttDisconnectedCb(uint32_t *args) {
 
-	MQTT_Client* client = (MQTT_Client*)args;
-	INFO("MQTT: Disconnected\r\n");
+  MQTT_Client* client = (MQTT_Client*) args;
+  INFO("MQTT: Disconnected\n");
 }
 
 void ICACHE_FLASH_ATTR
 mqttPublishedCb(uint32_t *args) {
 
-	MQTT_Client* client = (MQTT_Client*)args;
-	INFO("MQTT: Published\r\n");
+  MQTT_Client* client = (MQTT_Client*) args;
+  INFO("MQTT: Published\n");
 }
 
 void ICACHE_FLASH_ATTR
 mqttDataCb(uint32_t *args, const char* topic, uint32_t topic_len, const char *data, uint32_t data_len) {
-	char *topicBuf = (char*)os_zalloc(topic_len+1),
-			*dataBuf = (char*)os_zalloc(data_len+1);
 
-	MQTT_Client* client = (MQTT_Client*)args;
+  char *topicBuf = (char*) os_zalloc(topic_len + 1), *dataBuf =
+      (char*) os_zalloc(data_len + 1);
 
-	os_memcpy(topicBuf, topic, topic_len);
-	topicBuf[topic_len] = 0;
+  MQTT_Client* client = (MQTT_Client*) args;
 
-	os_memcpy(dataBuf, data, data_len);
-	dataBuf[data_len] = 0;
+  os_memcpy(topicBuf, topic, topic_len);
+  topicBuf[topic_len] = 0;
 
-	INFO("Receive topic: %s, data: %s \r\n", topicBuf, dataBuf);
-	os_free(topicBuf);
-	os_free(dataBuf);
+  os_memcpy(dataBuf, data, data_len);
+  dataBuf[data_len] = 0;
+
+  INFO("Receive topic: %s, data: %s \n", topicBuf, dataBuf);
+  os_free(topicBuf);
+  os_free(dataBuf);
 }
 
 //Do nothing function
@@ -114,10 +115,12 @@ void ICACHE_FLASH_ATTR
 setup() {
   CFG_Load();
 
-  MQTT_InitConnection(&mqttClient, sysCfg.mqtt_host, sysCfg.mqtt_port, sysCfg.security);
+  MQTT_InitConnection(&mqttClient, sysCfg.mqtt_host, sysCfg.mqtt_port,
+      sysCfg.security);
   //MQTT_InitConnection(&mqttClient, "192.168.11.122", 1880, 0);
 
-  MQTT_InitClient(&mqttClient, sysCfg.device_id, sysCfg.mqtt_user, sysCfg.mqtt_pass, sysCfg.mqtt_keepalive, 1);
+  MQTT_InitClient(&mqttClient, sysCfg.device_id, sysCfg.mqtt_user,
+      sysCfg.mqtt_pass, sysCfg.mqtt_keepalive, 1);
   //MQTT_InitClient(&mqttClient, "client_id", "user", "pass", 120, 1);
 
   MQTT_InitLWT(&mqttClient, "/lwt", "offline", 0, 0);
@@ -129,8 +132,7 @@ setup() {
   WIFI_Connect(sysCfg.sta_ssid, sysCfg.sta_pwd, wifiConnectCb);
 }
 
-void user_init(void)
-{
+void user_init(void) {
   // Initialize the GPIO subsystem.
   gpio_init();
 
@@ -144,7 +146,8 @@ void user_init(void)
   os_timer_arm(&setup_timer, 1000, false);
 
   //Start os task
-  system_os_task(user_procTask, user_procTaskPrio, user_procTaskQueue, user_procTaskQueueLen);
+  system_os_task(user_procTask, user_procTaskPrio, user_procTaskQueue,
+      user_procTaskQueueLen);
 
-	INFO("\r\nSystem started ...\r\n");
+  INFO("\nSystem started ...\n");
 }
